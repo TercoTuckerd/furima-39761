d39d854cfa9b2ba44f132e3451c06ac76efadcc3
TercoTuckerd <teru-takanori14@jcom.home.ne.jp>
2023-11-19 15:06:16 +0900

    item単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :wait_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :image, presence: true, unless: :was_attached?
 
   def was_attached?
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できる場合' do
+      it "全ての項目が存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できない場合' do
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'explanationが空では登録できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'category_idが1では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが1では登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'charge_idが1では登録できない' do
+        @item.charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge can't be blank")
+      end
+      it 'area_idが1では登録できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'wait_idが1では登録できない' do
+        @item.wait_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Wait can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが299以下では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが10000000以上では登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
