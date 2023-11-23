3422082f8611d0572d31217b7b87eb4a05893e73
TercoTuckerd <teru-takanori14@jcom.home.ne.jp>
2023-11-23 20:54:04 +0900

    編集機能の修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    unless item.user_id == current_user.id
+    unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
