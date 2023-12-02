03cfd11ae604391a4bcb94bb7ec4e812b39d6754
TercoTuckerd <teru-takanori14@jcom.home.ne.jp>
2023-12-02 10:44:21 +0900

    商品購入機能の修正2

--- app/models/buyer_order.rb
+++ app/models/buyer_order.rb
@@ -5,16 +5,16 @@ class BuyerOrder
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :token, presence: true
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :city, presence: true
-    validates :address, presence: true
+    validates :city
+    validates :address
     validates :phone, format: { with: /\A\d{10,11}\z/, length: {maximum: 11}, allow_blank: true, message: "is invalid. Input only number"}
   end
 
   def save(params,user_id)
-    order = Order.create(item_id: params[:item_id], user_id: user_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Buyer.create(post_code: post_code, area_id: area_id, city: city, address: address, building: building, phone: phone, order_id: order.id)
   end
 end
